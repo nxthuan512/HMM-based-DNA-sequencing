ea932a4a61f7c7a4f09c202e965685ae06e05cc4
nxthuan512 <nxthuan512@gmail.com>
2017-11-22 21:47:21 +0900

    Add files via upload

--- sensorSimulator.m
+++ sensorSimulator.m
@@ -1,14 +1,16 @@
-function sensorSimulator
+function sensorSimulator(SNR)
     % Generate a random DNA sequence
-    DNASeqLen = 100;    % Number of bases
-    DNASeq = generateDNASeq(DNASeqLen);
-  
+    % DNASeqLen = 100;    % Number of bases
+    % DNASeq = generateDNASeq(DNASeqLen);
+    
+    DNASeq = 'CATCCCTCACCTGAAGTGTCCAGCAAATACACCAAGGGTGACGCAGGACAAGCATGAGCCATTCATACTGCTGCAACCAGAGAGAGGGAGCAGGAAAAT';
+    DNASeqLen = length(DNASeq);
+    
     % Convert DNA to digital signal
     K = 3; % K-mer
     digitSignal = dnaSeq2Digit(DNASeq, K);
     
     % Add noise
-    SNR = 20;
     rawSensor = addNoise(digitSignal, SNR);
     
     % Plot the signal
